dbfec34a08a2e327d263010898f1db810f57e6d5
Uzoma A Orji <uzoma@uorjilaptop.(none)>
2014-01-18 20:11:41 -0500

    Updated admin.py

--- TheDive/admin.py
+++ TheDive/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Dumpster, FoodGroup, Food
+from TheDive.models import Dumpster, FoodGroup, Food
 
 # Register your models here.
 admin.site.register(Dumpster)
