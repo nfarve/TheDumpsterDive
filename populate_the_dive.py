78c852fdeadb47038465437d9ac9e520b992a052
Uzoma A Orji <uzoma@uorjilaptop.(none)>
2014-01-18 20:47:00 -0500

    Populated database

--- populate_the_dive.py
+++ populate_the_dive.py
@@ -6,21 +6,43 @@ def populate():
         lng=float_to_decimal(-71.09476),
         des='LaVerdes dumpster behind the MIT Student Center')
 
+    wholefood_dumpster = add_dumpster(lat=float_to_decimal(42.36186),
+        lng=float_to_decimal(-71.11427),
+        des='Whole Foods dumpster')
+
+    traderjoe_dumpster = add_dumpster(lat=float_to_decimal(42.35807),
+        lng=float_to_decimal(-71.11425),
+        des='Trader Joe dumpster')
+
     produce = add_foodgroup(name='Produce')
     meat = add_foodgroup(name='Meat')
     dairy = add_foodgroup(name='Dairy')
 
+    food1 = add_food(name='Broccoli', d=laverde_dumpster, 
+        fg=produce)
+    food2 = add_food('Kobe beef', laverde_dumpster, meat)
+    food3 = add_food('Goat cheese', laverde_dumpster, dairy)
+
+    food4 = add_food('Asparagus', wholefood_dumpster, produce)
+    food5 = add_food('Pork tenderloin', wholefood_dumpster, meat)
+    food6 = add_food('Almond milk', wholefood_dumpster, dairy)
+
+    food7 = add_food('Pomegranate', traderjoe_dumpster, produce)
+    food8 = add_food('Venizon', traderjoe_dumpster, meat)
+    food9 = add_food('Gouda cheese', traderjoe_dumpster, dairy)
+
+
 def add_dumpster(lat, lng, des):
     d = Dumpster.objects.get_or_create(latitude=lat, longitude=lng, 
-        description=des)
+        description=des)[0]
     return d
 
 def add_foodgroup(name):
-    fg = FoodGroup.objects.get_or_create(name=name)
+    fg = FoodGroup.objects.get_or_create(name=name)[0]
     return fg
 
-def add_food(name,dr,fg):
-    f = Food.objects.get_or_create(name=name, dumpster=d, foodgroup=fg)
+def add_food(name,d,fg):
+    f = Food.objects.get_or_create(name=name, dumpster=d, foodgroup=fg)[0]
     return f
 
 def float_to_decimal(f):
